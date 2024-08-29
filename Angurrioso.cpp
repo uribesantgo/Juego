0d0f409903bf5a1ce81b60b73a4c121a483eb70f
santgo <santiuribe04176@gmail.com>
2024-08-29 17:24:21 -0500

    Actualizacion1

--- Angurrioso.cpp
+++ Angurrioso.cpp
@@ -1,6 +1,40 @@
 
 #include <iostream>
+
 using namespace std;
+
+class juego{
+
+    public:
+    int maxpuntos;
+    int jugadores;
+    
+
+    juego (int maxpuntos, int jugadores){
+
+    };
+    
+
+};
+
+class Jugador{
+
+public:
+int puntosRonda;
+string nombre;
+int puntosTotal;
+
+void turno(){
+
+};
+
+};
+
+
+
+
+
+
 
 
 
